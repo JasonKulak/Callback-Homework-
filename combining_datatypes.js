8021548aa7fa5661b846a1c4d1cd5fcdab31613a
Jason Kulak <JasonKulak@yahoo.com>
2023-11-24 17:27:46 -0600

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -4,9 +4,9 @@
 
 //Create an object, called crayonBox, that has a property that is an array. The array should have the names of some crayons. Log one of the elements of that array.
 
-const crayonBox = ['red', 'green', 'blue', 'orange', 'white', 'black', 'velvet'];
+//const crayonBox = ['red', 'green', 'blue', 'orange', 'white', 'black', 'velvet'];
 
-console.log(crayonBox[1])
+//console.log(crayonBox[1])
 
 //Create an object bottle that has a property that is an object: cap. cap can have properties like material: 'metal' or 'plastic', color: blue or white etc. Log one of the properties of that inner object.
 
@@ -43,3 +43,38 @@ const apartmentBuilding = [
     ]
 ]
 console.log(apartmentBuilding[0][3])
+
+/////////////////////////////////////////////////////////////////
+//Combine objects, arrays, and functions more than one level deep
+/////////////////////////////////////////////////////////////////
+
+//Create a function knit that returns an object that has the following kinds of properties item: scarf, size : 6ft etc. Log a value of that object (hint: call the function and then call a property on the return value).
+
+function knit() {
+    const coldWeatherClothes = {
+        item: 'scarf',
+        size: '6ft',
+        material: 'yarn',
+        color: 'red'
+    }
+    return coldWeatherClothes.item
+}
+console.log(knit())
+
+//Create a function crayonSelector that returns an object that has an array (you can reuse your crayonBox object). Log one of the elements of that array.
+
+function crayonSelector() {
+    const crayonBox = ['red', 'green', 'blue', 'orange', 'white', 'black', 'velvet'];
+    return crayonBox[5]
+}
+console.log(crayonSelector())
+
+//Create a function powerButton that returns a function called options - options should console.log a simple message like select a song. Call that inner function.
+
+function powerButton() {
+    function options() {
+    console.log('select a song');
+    }
+    return options()
+}
+powerButton()
