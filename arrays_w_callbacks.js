067a1cfe990542b769bebb6420be88fde8add02f
Jason Kulak <JasonKulak@yahoo.com>
2023-11-25 16:52:06 -0600

    callbacks

--- arrays_w_callbacks.js
+++ arrays_w_callbacks.js
@@ -2,20 +2,79 @@
 //Model a venging machine
 /////////////////////////
 
-const vendingMachine = {
-     snacks: [
-        {name: 'chips', price: 1},
-        {name: 'chocolate', price: 2},
-        {name: 'cookies', price: 3},
-    ],
-    vend: (num) => { 
-        if (num === 0) {
-            console.log(vendingMachine.snacks[0].name);
-        } else if (num === 1) {
-            console.log(vendingMachine.snacks[1].name);
-        } else { (num === 2) 
-            console.log(vendingMachine.snacks[2].name);
-        }
-    }
+// const vendingMachine = {
+//      snacks: [
+//         {name: 'chips', price: 1},
+//         {name: 'chocolate', price: 2},
+//         {name: 'cookies', price: 3},
+//     ],
+//     vend: (num) => { 
+//         if (num === 0) {
+//             console.log(vendingMachine.snacks[0].name);
+//         } else if (num === 1) {
+//             console.log(vendingMachine.snacks[1].name);
+//         } else { (num === 2) 
+//             console.log(vendingMachine.snacks[2].name);
+//         }
+//     }
+// }
+// vendingMachine.vend(0);
+
+///////////
+//Callbacks
+///////////
+//Make a function add that takes two arguments (numbers) and sums them together
+
+// function add(a, b) {
+//     return a + b;
+// }
+// let n1 = 10;
+// let n2 = 20;
+// let sum = add(n1, n2);
+// console.log(sum);
+
+function add(a, b) {
+    console.log(a + b);
 }
-vendingMachine.vend(0);
+add(1, 2)
+
+//Make a function subtract that takes two arguments (numbers) and subtracts them
+
+function subtract(x, y) {
+    console.log(x - y);
+}
+subtract(15, 5)
+
+//Make a function multiply that takes two arguments and multiplies them
+
+function multiply(j, k) {
+    console.log(j * k);
+}
+multiply(2, 4)
+
+//Make a function divide that takes two arguments and divides them
+
+function divide(w, v) {
+    console.log(w / v);
+}
+divide(50, 25)
+
+//Make a function calculate that takes three arguments. Assume the two arguments are a number ie num1, num2 and a function called operates (a callback).
+//version1
+// function calculate(operates(num1, num2)){
+//     return value;
+//     let value = operates(1 + 2);
+// }
+// calculate(operates)
+
+//version2
+function calculate(operates) {
+    return operates(5, 4)
+}
+console.log(calculate((a, b) => a + b))
+console.log(calculate((a, b) => a - b))
+console.log(calculate((a, b) => a * b))
+console.log(calculate((a, b) => a / b))
+
+//Q??????????????????^^^
+
